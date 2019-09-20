e0d7f4d327aa98fd635b589ccd29fed0db925308
thisishoon <jihoon522@naver.com>
2019-09-20 15:56:43 +0900

    delete token after move_script_to_user

--- user/views.py
+++ user/views.py
@@ -6,7 +6,11 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from script.models import SpeechScript
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework_jwt.utils import jwt_decode_handler
+
 from django.contrib.auth.models import User
+import json
+from rest_framework.authtoken.models import Token
+
 
 # Create your views here.
 def show_user_info(request):
@@ -18,20 +22,28 @@ def show_user_info(request):
         decoded_payload = jwt_decode_handler(jwt[4:])
         user = User.objects.get(id=decoded_payload['user_id'])
 
-        print(request.user.email)
-        print(user.email)
-
         return JsonResponse({"email": user.email, "first_name": user.first_name,
                              "last_name": user.last_name})
+
 
 @csrf_exempt
 def move_script_to_user(request):
     if request.method == "POST":
-        token = request.data["token"]
-        jwt = request.data["jwt"]
 
-        temp1 = SpeechScript.objects.filter(user=token.user)
-        temp2 = SpeechScript.objects.filter(user=jwt.user)
-        print(temp1)
-        print(temp2)
-        return
+        token = json.loads(request.body)['token']
+        jwt = json.loads(request.body)['jwt']
+
+        try:
+            decoded_token = Token.objects.get(key=token)
+        except Token.DoesNotExist:
+            return HttpResponse(status=400)
+
+        decoded_payload = jwt_decode_handler(jwt)
+
+        token_script = SpeechScript.objects.filter(user=decoded_token.user_id)
+        jwt_user = User.objects.get(id=decoded_payload["user_id"])
+
+        token_script.update(user=jwt_user)
+        decoded_token.delete()
+
+        return HttpResponse(status=200)
